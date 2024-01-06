149ac027e8e529ee7b9e798028538ce8df37022a
Renan Augusto <renancandido82@outlook.com>
2024-01-06 11:02:10 -0300

    So corrigindo a pagina de atividades pt.2

--- PUBLIC/PAGES/JS/activities.js
+++ PUBLIC/PAGES/JS/activities.js
@@ -5,11 +5,19 @@ fetch("/GETEXERCISES")
     .then(response => response.json())
         .then(data => {
             console.log(data);
-            for(var i = 0; i < data.Exercies.length; i++){
-                div +=`<div class="exercicies"><h1>${data.Exercies[i].name}</h1><h5>Prof.${data.Exercies[i].prof}</h5><br><h6>${data.Exercies[i].date}</h6><br><br><button>Abrir</button></div>`;
+            if(data.Exercies.length > 0){
+                for(var i = 0; i < data.Exercies.length; i++){
+                    div +=`<div class="exercicies"><h1>${data.Exercies[i].name}</h1><h5>Prof.${data.Exercies[i].prof}</h5><br><h6>${data.Exercies[i].date}</h6><br><br><button>Abrir</button></div>`;
+                }
+                main.innerHTML = div.replace('undefined', '');
+                setTimeout(function(){
+                    loading.style.display = 'none';
+                }, 2000);
+            } else {
+                main.innerHTML = "<h1>Sem atividades para hoje!</h1>";
+                setTimeout(function(){
+                    loading.style.display = 'none';
+                }, 2000);
             }
-            main.innerHTML = div.replace('undefined', '');
-            setTimeout(function(){
-                loading.style.display = 'none';
-            }, 2000);
+            
         });

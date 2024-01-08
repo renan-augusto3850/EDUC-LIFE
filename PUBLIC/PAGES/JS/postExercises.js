8c675629c4d4a51a4d099e85c446904461e6cc4d
Renan Augusto <renancandido82@outlook.com>
2024-01-07 21:18:50 -0300

    problemas resolvidos no activities.js pt.2

--- PUBLIC/PAGES/JS/postExercises.js
+++ PUBLIC/PAGES/JS/postExercises.js
@@ -3,7 +3,7 @@ function fetchData() {
     var prof = document.getElementById("prof");
     var content = document.getElementById("content");
     var date = new Date().toLocaleDateString();
-    fetch('/POSTEXERCICISES', {
+    fetch('/POSTEXERCISES', {
     method: 'POST',
     headers: {
         'Content-Type': 'application/json'
